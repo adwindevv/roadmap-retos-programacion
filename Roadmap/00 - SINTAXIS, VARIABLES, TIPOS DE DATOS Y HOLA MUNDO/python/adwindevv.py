9c014c3619e53e27214c0a02d3d40342b2edb7b4
adair03bb <adair8004@gmail.com>
2025-05-28 18:23:56 -0600

    Create adwindevv.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/adwindevv.py
@@ -0,0 +1,23 @@
+#https://www.python.org/
+#comentario de una linea
+"""
+Comentario
+de varias 
+lineas
+"""
+'''
+Comentario
+en varias
+lineas
+'''
+mi_variable = "Mi variable"
+MY_COONSTANT= "Mi constante"#por convencion
+
+my_int = 10
+my_float = 10.5
+my_string = "Mi cadena de texto con doble comilla"
+my_string = 'Mi cadena de texto con comilla simple'
+my_bool = True
+my_bool = False
+
+print("¡Hola, Python!")
